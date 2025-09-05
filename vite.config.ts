afd0e57dc6dfdcf867bbedf533c85181b7ac6e2b
Aravind R Pillai <aravindrpillai1992@gmail.com>
2025-09-05 15:17:41 -0400

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    allowedHosts: ["aravindrpillai.com", "textbox.aravindrpillai.com", "chat.aravindrpillai.com", "tellme.aravindrpillai.com"],
+    host: "0.0.0.0",
     port: 80,
   },
   plugins: [
