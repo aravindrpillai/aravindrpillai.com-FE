b0d81fc3fc0172f81d59fa51d15a0ccc8f6767c5
Aravind R Pillai <aravindrpillai1992@gmail.com>
2025-09-05 17:26:09 -0400

    textbox fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuickChat from "./pages/qchat/QuickChat";
 
-// import Home from "./pages/home/Home";
-// import Anonymous from "./pages/anonymous/Index";
-// import ReadAnonymous from "./pages/anonymous/Read";
-// import TextBoxPage from "./pages/textbox/Index";
+import Home from "./pages/home/Home";
+import Anonymous from "./pages/anonymous/Index";
+import ReadAnonymous from "./pages/anonymous/Read";
+import TextBoxPage from "./pages/textbox/Index";
 
 const queryClient = new QueryClient();
 
@@ -19,16 +19,14 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          
+
           <Route path="qchat/:name?/:sender?" element={<QuickChat />} />
-{/*           
+
           <Route path="anonymous" element={<Anonymous />} />
           <Route path="anonymous/read" element={<ReadAnonymous />} />
-          
           <Route path="textbox/:code?" element={<TextBoxPage />} />
-        
+
           <Route path="/" element={<Home />} />
-         */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
